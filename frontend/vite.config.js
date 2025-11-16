462050e1462c902e6b76e409808203717cd7a226
Rayen Brahem <brahemrayen03@gmail.com>
2025-11-16 20:53:07 +0100

    LINKUPDATE FE

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://dokanzemni-be.2bj94x.easypanel.host/',
         changeOrigin: true
       }
     }
